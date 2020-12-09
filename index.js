0abc25977001ebb141cb52862bc1f285fb8845ae
k2hyun4 <k2hyun4@naver.com>
2020-12-09 20:12:52 +0900

    feat : npm init & express routing

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
+const morgan = require('morgan');
 
-function commonMiddleware(req, res, next) {
-    console.log('common middleware');
-    next(new Error('Error occuered'));
-}
+const users = [
+    { id: 1, name: 'alice' }, 
+    { id: 2, name: 'bek' }, 
+    { id: 3, name: 'chris' }, 
+];
 
-function errorMiddleware(err, req, res, next) {
-    console.log(err.message);
-    next();
-}
-
-function logger(req, res, next) {
-    console.log('i am logger');
-    next();
-}
-
-function logger2(req, res, next) {
-    console.log('i am logger2');
-    next();
-}
-
-
-app.use(logger);
-app.use(commonMiddleware);
-app.use(errorMiddleware);
-app.use(logger2);
 app.use(morgan('dev'));
 
+app.get('/users', function(req, res) {
+    res.json(users);
+});
+
 app.listen(3000, function() {
-    console.log(`Server is running in port ${3000}`);
-})
+});
+
