6f52fcf13e24b3fdcc5e0cba9b03f3d4eb7ccd98
k2hyun4 <k2hyun4@naver.com>
2020-12-09 18:30:43 +0900

    feat : express.js init

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const { SSL_OP_NO_TLSv1_1 } = require('constants');
-const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
-//const data = fs.readFileSync('./data.txt', 'utf8');
-//console.log(data);
-
-const data = fs.readFile('./data.txt', 'utf8', function(err, data) {
-    console.log(data);
-})
-
-for (var i = 0; i < 10; i++) {
-    console.log(i);
+function commonMiddleware(req, res, next) {
+    console.log('common middleware');
+    next(new Error('Error occuered'));
 }
+
+function errorMiddleware(err, req, res, next) {
+    console.log(err.message);
+    next();
+}
+
+function logger(req, res, next) {
+    console.log('i am logger');
+    next();
+}
+
+function logger2(req, res, next) {
+    console.log('i am logger2');
+    next();
+}
+
+
+app.use(logger);
+app.use(commonMiddleware);
+app.use(errorMiddleware);
+app.use(logger2);
+app.use(morgan('dev'));
+
+app.listen(3000, function() {
+    console.log(`Server is running in port ${3000}`);
+})
